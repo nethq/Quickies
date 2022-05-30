42700ecedc519d0d8eda7af5a45aebe8fa020243
NethQ <NStoykov2001@protonmail.com>
2022-05-30 18:58:09 +0300

    Quickies - v.1.0.69.

---
+++ Quckies/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Quckies
+{
+    partial class Quickies
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(796, 437);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(788, 409);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main Menu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(788, 409);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(419, 170);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(363, 139);
+            this.listBox2.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(31, 170);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(382, 139);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(31, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 53);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save Config";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(549, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove Quickie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Quickie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Quickies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Quickies";
+            this.Text = "Quickies";
+            this.ResizeEnd += new System.EventHandler(this.Quickies_ResizeEnd);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Quckies/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace Quckies
+{
+    public partial class Quickies : Form
+    {
+        int btnCount = 0;
+        List<string> dynamicButtons = new List<string>();
+        public Quickies()
+        {
+            InitializeComponent();
+            if (System.IO.File.Exists(Application.StartupPath + "\\execPaths.txt"))
+            {
+                foreach (var line in System.IO.File.ReadAllLines(Application.StartupPath+"\\execPaths.txt"))
+                {
+                    createQuickie(btnCount, line);
+                    btnCount++;
+
+                }
+            }
+        }
+
+        private void SaveConfig()
+        {
+            string fname = "execPaths.txt";
+            fname = Application.StartupPath + @"\" + fname;
+//            MessageBox.Show(fname);
+            System.IO.File.Delete(fname);
+            System.IO.File.WriteAllLines(fname, dynamicButtons);
+        }
+
+        private void createQuickie(int index,string execPath)
+        {
+            Button dynamicButton = new Button();
+            // Set Button properties 
+            dynamicButton.Height = 40;
+            dynamicButton.Width = 120;
+            dynamicButton.Location = new Point((index/7)*(dynamicButton.Width+20),(index%7)*(dynamicButton.Height+20));
+            // Set background and foreground  
+            if (System.IO.File.Exists(execPath))
+            {
+             //   dynamicButton.Image = Image.FromFile(execPath);
+            }
+            dynamicButton.Text = execPath.Remove(0, execPath.LastIndexOf('\\') + 1); 
+            dynamicButton.Name = execPath;
+            dynamicButton.Font = new Font("Georgia", 8);
+            dynamicButton.Click += new EventHandler(DynamicButton_Click);
+            dynamicButtons.Add(execPath);
+            tabPage1.Controls.Add(dynamicButton);
+            listBox1.Items.Add(execPath);
+        }
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            string temp = (sender as Button).Name;
+            if (System.IO.File.Exists(temp))
+            {
+                new Process { StartInfo = new ProcessStartInfo(temp) { UseShellExecute = true } }.Start();
+            }
+            else
+            {
+                MessageBox.Show("File : " + temp + " not found!");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//opens dialogbox to create a quick action
+            string temp = "Creating a quickie bound to : " + textBox1.Text;
+            MessageBox.Show(temp);
+            createQuickie(btnCount,textBox1.Text.ToString());
+            btnCount++;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {//remove quickie
+            string temp = listBox1.Text;
+            dynamicButtons.RemoveAt(dynamicButtons.IndexOf(temp));
+            listBox1.Items.RemoveAt(listBox1.Items.IndexOf(temp));
+            MessageBox.Show("Removed : |" + temp + "|");
+            SaveConfig();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            textBox1.Text = dlg.FileName;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {//save button config                        
+            SaveConfig();
+        }
+
+        private void Quickies_ResizeEnd(object sender, EventArgs e)
+        {
+            tabControl1.Size = this.Size;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            foreach(var line in dynamicButtons)
+            {
+                listBox2.Items.Add(line);
+            }
+        }
+    }
+}
